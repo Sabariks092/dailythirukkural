ab613fb873d27f88b5e01294b8ff5d54c7a701fc
samks0409 <samdev0418@gmail.com>
2026-03-12 12:53:11 +0530

    calendarheatmap2 components typoe error fix

--- src/components/analytics/CalendarHeatmap2.tsx
+++ src/components/analytics/CalendarHeatmap2.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useMemo, useEffect, useRef } from "react";
 import type { DailyProgress } from "../../types";
 import { getTodayIST, formatDate } from "../../utils/dateUtils";
+import type { JSX } from "react";
 
 /**
  * CUSTOM GITHUB-STYLE HEATMAP BY MONTH
